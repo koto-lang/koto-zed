60e11db3a11a506385c05c3e68597800c42a714c
Roberto Saccon <rsaccon@gmail.com>
2025-04-08 06:36:51 +0100

    small Config  fixes

---
+++ languages/koto/brackets.scm
@@ -0,0 +1,8 @@
+; makes extenson NOT working
+;
+; ("[" @open "]" @close)
+; ("{" @open "}" @close)
+; ("(" @open ")" @close)
+; ("|" @open "|" @close)
+; ("\"" @open "\"" @close)
+; ("'" @open "'" @close)
