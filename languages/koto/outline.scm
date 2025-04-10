041e6928a453f55669f42d509f3bfe3b5a2a424e
Roberto Saccon <rsaccon@gmail.com>
2025-04-10 03:46:42 +0100

    bug fixes and updated README

---
+++ languages/koto/outline.scm
@@ -0,0 +1,13 @@
+; (function
+;     name: (_) @name) @item
+
+; (module_attribute
+;     "module" @context
+;     name: (_) @name) @item
+;
+; (function_signature
+;     name: (_) @name) @item
+;
+; (enum_declaration
+;     "enum" @context
+;     name: (_) @name) @item
